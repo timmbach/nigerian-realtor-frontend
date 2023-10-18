8679740194543592f725be14626bada800908e74
timmbach <follow.timothy@gmail.com>
2023-10-18 08:07:41 +0100

    updated header and mase the profile page private

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
@@ -32,11 +34,21 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="cursor-pointer text-slate-700 hover:font-medium">
-              Sing In
-            </li>
-          </Link>
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.profileImg}
+                alt="user img"
+              ></img>
+            </Link>
+          ) : (
+            <Link to="/sign-in">
+              <li className="cursor-pointer text-slate-700 hover:font-medium">
+                Sing In
+              </li>
+            </Link>
+          )}
         </ul>
       </div>
     </header>
