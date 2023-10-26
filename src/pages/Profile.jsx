0bca92ca5b0ef9a99c20f1b034096c3b1b4cd008
timmbach <follow.timothy@gmail.com>
2023-10-26 13:25:55 +0100

    complete delete user listings functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -51,6 +51,7 @@ export default function Profile() {
     if (file) {
       handleFileUpload(file);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [file]);
 
   const handleFileUpload = (file) => {
@@ -160,24 +161,24 @@ export default function Profile() {
     }
   };
 
-  // const handleListingDelete = async (listingId) => {
-  //   try {
-  //     const res = await fetch(`/api/listing/delete/${listingId}`, {
-  //       method: "DELETE",
-  //     });
-  //     const data = await res.json();
-  //     if (data.success === false) {
-  //       console.log(data.message);
-  //       return;
-  //     }
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
 
-  //     setUserListings((prev) =>
-  //       prev.filter((listing) => listing._id !== listingId)
-  //     );
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
@@ -296,7 +297,10 @@ export default function Profile() {
                 </Link>
 
                 <div className="flex flex-col items-center">
-                  <button className="text-red-700 font-semibold text-xs uppercase">
+                  <button
+                    onClick={() => handleListingDelete(listing._id)}
+                    className="text-red-700 font-semibold text-xs uppercase"
+                  >
                     Delete
                   </button>
                   <Link to={`/update-listing/${listing._id}`}>
